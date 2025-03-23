1ec7befaab76f313a85ebfb9f06b0e43925e2bec
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-23 19:02:18 +0000

    saving on the localstorage and project ended

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -11,7 +11,35 @@ function addTask() {
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li)
 
+    let span = document.createElement("span")
+    span.innerHTML = "\u00d7";  // x icon
+    li.appendChild(span)
+
   }
 
   inputBox.value = '';
+  saveData()
 }
+
+
+listContainer.addEventListener("click", function(e) {
+  if(e.target.tagName === "LI") {
+    e.target.classList.toggle("checked")
+    saveData()
+
+  } else if(e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData()
+  }
+}, false)
+
+
+function saveData() {
+  localStorage.setItem('data', listContainer.innerHTML)
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem('data')
+}
+
+showTask()
